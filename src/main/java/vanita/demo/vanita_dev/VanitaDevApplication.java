7d9036de370110b85123bff65aedcd33beaa7268
Rajkumar k <kanche.rajkumar@sakhatech.com>
2024-06-27 15:33:21 +0530

    Dev application , added index.html

---
+++ src/main/java/vanita/demo/vanita_dev/VanitaDevApplication.java
@@ -0,0 +1,13 @@
+package vanita.demo.vanita_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanitaDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanitaDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vanita/demo/vanita_dev/VanitaDevApplicationTests.java
@@ -0,0 +1,13 @@
+package vanita.demo.vanita_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanitaDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
